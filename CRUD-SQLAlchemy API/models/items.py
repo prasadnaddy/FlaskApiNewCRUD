313fde0affae55265f6c7952be63e433e33fe5b5
PrasadNadiger <prasad@unitypophealth.com>
2020-08-22 13:59:09 -0400

    changes to foreignkeys

--- CRUD-SQLAlchemy API/models/items.py
+++ CRUD-SQLAlchemy API/models/items.py
@@ -13,8 +13,11 @@ class ItemModel(db.Model):        #model for representing the Items
     storename = db.Column(db.String(100), db.ForeignKey(StoreModel.name))       #foreign key mapping to stores id key column
     storeid = db.Column(db.Integer, db.ForeignKey(StoreModel.id))       #foreign key mapping to stores id key column
     store = db.relationship('StoreModel')       #creating relationship with Stores table by using relationship
-    __table_args__ = (db.UniqueConstraint('storename', 'storeid',name='_stores_items_uc'),      #adding unique constraint for foreign key in postgressql
-                 )
+    __table_args__ = (db.ForeignKeyConstraint([storename, storeid],
+                                           [StoreModel.name, StoreModel.id]),
+                      {}, db.UniqueConstraint('storename', 'storeid',name='_stores_items_uc'),)
+    # __table_args__ = (db.UniqueConstraint('storename', 'storeid',name='_stores_items_uc'),      #adding unique constraint for foreign key in postgressql
+    #              )
     def __init__(self, name, price, type, uniqueid, storename):
         super().__init__()
         self.name = name
